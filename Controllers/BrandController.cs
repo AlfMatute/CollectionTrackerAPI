10757ab3ee8298a4c82ff945f9fe08e8b027badd
AlfMatute <alf.matute@gmail.com>
2020-07-07 12:45:26 -0600

    Controllers for brand and category added

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CollectionTrackerAPI.Models;
+using CollectionTrackerAPI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CollectionTrackerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BrandController : ControllerBase
+    {
+        private readonly ILogger<BrandController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public BrandController(ILogger<BrandController> logger, ApplicationDbContext applicationDbContext, IMapper mapper)
+        {
+            _logger = logger;
+            _context = applicationDbContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<BrandViewModel> Get(int id)
+        {
+            try
+            {
+                var brand = _context.Brands.Where(b => b.BrandId == id).FirstOrDefault();
+                if(brand != null)
+                {
+                    return Ok(_mapper.Map<Brand, BrandViewModel>(brand));
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error when getting the brand: {ex.Message}");
+                return BadRequest($"Error when getting the brand: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<BrandViewModel>> Get()
+        {
+            try
+            {
+                var brands = _context.Brands.ToList();
+                if(brands != null)
+                {
+                    return Ok(_mapper.Map<IEnumerable<Brand>, IEnumerable<BrandViewModel>>(brands));
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Error when getting brands: {ex.Message}");
+                return BadRequest($"Error when getting brands: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<BrandViewModel> Create([FromBody]BrandViewModel model)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    var newBrand = _mapper.Map<BrandViewModel, Brand>(model);
+                    _context.Add(newBrand);
+                    if(_context.SaveChanges() == 0)
+                    {
+                        return Created($"/api/Brand/{newBrand.BrandId}", _mapper.Map<Brand, BrandViewModel>(newBrand));
+                    }
+                    else
+                    {
+                        return BadRequest("Failed to create the brand");
+                    }
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Error when creating a new brand: {ex.Message}");
+                return BadRequest($"Error when creating a new brand: {ex.Message}");
+            }
+        }
+
+        [HttpPut]
+        public ActionResult<BrandViewModel> Put([FromBody]BrandViewModel model)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    var updateBrand = _mapper.Map<BrandViewModel, Brand>(model);
+                    _context.Update(updateBrand);
+                    if(_context.SaveChanges() == 0)
+                    {
+                        return Ok(_mapper.Map<Brand, BrandViewModel>(updateBrand));
+                    }
+                    else
+                    {
+                        return BadRequest("Failed to update the brand");
+                    }
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Error when updating the brand: {ex.Message}");
+                return BadRequest($"Error when updating the brand: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<BrandViewModel> Delete(int id)
+        {
+            try
+            {
+                var deleteBrand = _context.Brands.Where(b => b.BrandId == id).FirstOrDefault();
+                if(deleteBrand != null) 
+                {
+                    _context.Remove(deleteBrand);
+                    if(_context.SaveChanges() == 0)
+                    {
+                        return Ok(_mapper.Map<Brand, BrandViewModel>(deleteBrand));
+                    }
+                    else
+                    {
+                        return BadRequest("Failed to delete the brand");
+                    }
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Error when deleting the brand: {ex.Message}");
+                return BadRequest($"Error when deleting the brand: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CollectionTrackerAPI.Models;
+using CollectionTrackerAPI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+
+namespace CollectionTrackerAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ILogger<CategoryController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ILogger<CategoryController> logger, ApplicationDbContext context, IMapper mapper)
+        {
+            _logger = logger;
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<CategoryViewModel> Get(int id)
+        {
+            try
+            {
+                var category = _context.Categories.Where(c => c.CategoryId == id).FirstOrDefault();
+                if(category != null)
+                {
+                    return Ok(_mapper.Map<Category, CategoryViewModel>(category));
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = $"Error when getting the category: {ex.Message}";
+                _logger.LogError(error);
+                return BadRequest(error);
+            }
+        }
+
+        [HttpGet("{active:bool}")]
+        public ActionResult<IEnumerable<CategoryViewModel>> Get(bool active)
+        {
+            try
+            {
+                var categories = _context.Categories.Where(c => c.Active == active).ToList();
+                if(categories != null)
+                {
+                    return Ok(_mapper.Map<IEnumerable<Category>, IEnumerable<CategoryViewModel>>(categories));
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+                string errror = $"Error when getting the Categories: {ex.Message}";
+                _logger.LogError(errror);
+                return BadRequest(errror);
+            }
+        }
+
+        [HttpPut]
+        public ActionResult<CategoryViewModel> Put([FromBody] CategoryViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var updateCategory = _mapper.Map<CategoryViewModel, Category>(model);
+                    _context.Update(updateCategory);
+                    if (_context.SaveChanges() == 0)
+                    {
+                        return Ok(_mapper.Map<Category, CategoryViewModel>(updateCategory));
+                    }
+                    else
+                    {
+                        return BadRequest($"Failed to update the Category");
+                    }
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = $"Error when updating Category: {ex.Message}";
+                _logger.LogError(error);
+                return BadRequest(error);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<CategoryViewModel> Create([FromBody]CategoryViewModel model)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    var newCategory = _mapper.Map<CategoryViewModel, Category>(model);
+                    _context.Add(newCategory);
+                    if(_context.SaveChanges() == 0)
+                    {
+                        return Created($"/api/Category/{newCategory.CategoryId}", _mapper.Map<Category, CategoryViewModel>(newCategory));
+                    }
+                    else
+                    {
+                        return BadRequest("Failed to save the new Category");
+                    }
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch(Exception ex)
+            {
+                string error = $"Error when creating a new Category: {ex.Message}";
+                _logger.LogError(error);
+                return BadRequest(error);
+            }
+        }
+
+        [HttpDelete("id:int")]
+        public ActionResult<CategoryViewModel> Delete(int id)
+        {
+            try
+            {
+                var deleteCategory = _context.Categories.Where(c => c.CategoryId == id).FirstOrDefault();
+                if(deleteCategory != null)
+                {
+                    _context.Remove(deleteCategory);
+                    if(_context.SaveChanges() == 0)
+                    {
+                        return Ok(_mapper.Map<Category, CategoryViewModel>(deleteCategory));
+                    }
+                    else
+                    {
+                        return BadRequest("Failed to delete the Category");
+                    }
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+                string error = $"Error when deleting the Category: {ex.Message}";
+                _logger.LogError(error);
+                return BadRequest(error);
+            }
+        }
+    }
+}
