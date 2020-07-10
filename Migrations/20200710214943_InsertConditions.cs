16b72f17008a3926d9566fd3db3d14fdbd36e987
AlfMatute <alf.matute@gmail.com>
2020-07-10 16:01:43 -0600

    Added Migration for initial Conditions data on the database

---
+++ Migrations/20200710214943_InsertConditions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CollectionTrackerAPI.Migrations
+{
+    public partial class InsertConditions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Conditions (Description, Active) VALUES ('New', 1), ('Renewed', 1), ('Rental', 1), ('Used - Like new or Open Box', 1), ('Used - Very Good', 1), ('Used - Good', 1), ('Used - Acceptable', 1)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Conditions  WHERE Description IN ('New', 'Renewed', 'Rental', 'Used - Like new or Open Box', 'Used - Very Good', 'Used - Good', 'Used - Acceptable') AND Active = 1");
+        }
+    }
+}
